1bb5d052a13bdb24fa8f580671bf51818031740f
wahoyu <645702818@qq.com>
2023-03-28 10:55:27 +0800

    3.28修改前记录

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
 import com.example.demo.util.Similarity;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
