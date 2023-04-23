aa462048777a8558c2b2223f626394b02b6d746c
wahoyu <645702818@qq.com>
2023-04-23 11:14:08 +0800

    添加备案号

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,17 +1,22 @@
 package com.example.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.demo.eneity.User;
 import com.example.demo.dao.UserDao;
+import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserDao dao;
+    @Resource
+    UserMapper mapper;
 
     public int getCount() {
         return dao.getCount();
@@ -29,10 +34,19 @@ public class UserServiceImpl implements UserService {
         return dao.findUser(page,limit,name);
     }
 
-    //
+    //用户注册
     public int addUser(User user) {
 
-        return dao.addUser(user);
+        //判断用户是否已经注册
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("phone",user.getPhone());
+
+        int i = Math.toIntExact(mapper.selectCount(wrapper));
+        if (i != 0){
+            return 0;
+        }
+        user.setStatus(1);
+        return mapper.insert(user);
     }
 
     public int updateUser(User user) {
