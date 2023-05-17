9f17bb59a22c96b0cfa42e0ebfd9f2d1ff25f03d
wahoyu <645702818@qq.com>
2023-05-17 09:08:17 +0800

    5.17公告删除修改结束

--- src/main/java/com/example/demo/eneity/Notice.java
+++ src/main/java/com/example/demo/eneity/Notice.java
@@ -1,5 +1,6 @@
 package com.example.demo.eneity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +14,15 @@ public class Notice {
     Integer id;
     String title;
     String content;
+
     Integer createBy;
     Date createTime;
     Integer updateBy;
     Date updateTime;
     Integer status;
+
+    @TableField(exist = false)
     Admin create_admin;
+    @TableField(exist = false)
     Admin update_admin;
 }
--- src/main/java/com/example/demo/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/NoticeServiceImpl.java
@@ -1,45 +1,93 @@
 package com.example.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.demo.eneity.Admin;
+import com.example.demo.eneity.Complain;
 import com.example.demo.eneity.Notice;
 import com.example.demo.dao.NoticeDao;
+import com.example.demo.eneity.User;
+import com.example.demo.mapper.AdminMapper;
+import com.example.demo.mapper.NoticeMapper;
 import com.example.demo.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class NoticeServiceImpl implements NoticeService {
     @Autowired
     NoticeDao dao;
+    @Resource
+    NoticeMapper mapper;
+
+    @Resource
+    AdminMapper adminMapper;
+
+    //获取notice表中的所有数据
     public int getCount() {
-        return dao.getCount();
+        return Math.toIntExact(mapper.selectCount(null));
     }
 
+    //对notice整张表进行分页查询
     public List<Notice> getAllNotices(int page, int limit) {
-        return dao.getAllNotices(page,limit);
+        //分页查询
+        Page<Notice> p = new Page<>(page, limit);
+        List<Notice> list = mapper.selectPage(p,null).getRecords();
+        System.out.println(list);
+        //如果list不为空,添加user
+        if (list!=null){
+            for (Notice notice:list){
+                QueryWrapper<Admin> wrapper = new QueryWrapper<>();
+                wrapper.eq("id", notice.getCreateBy());
+                Admin admin = adminMapper.selectOne(wrapper);
+                notice.setCreate_admin(admin);
+            }
+            for (Notice notice:list){
+                QueryWrapper<Admin> wrapper2 = new QueryWrapper<>();
+                wrapper2.eq("id", notice.getUpdateBy());
+                Admin admin = adminMapper.selectOne(wrapper2);
+                notice.setUpdate_admin(admin);
+            }
+            return list;
+        }else{
+            return null;
+        }
     }
 
+    //添加公告
     public int addNotice(Notice notice) {
-        return dao.addNotice(notice);
+        return mapper.insert(notice);
     }
 
+    //更新公告
     public int updateNotice(Notice notice) {
-        return dao.updateNotice(notice);
+        QueryWrapper<Admin> wrapper = new QueryWrapper<>();
+        wrapper.eq("id", notice.getUpdateBy());
+        Admin admin = adminMapper.selectOne(wrapper);
+        notice.setUpdate_admin(admin);
+        return mapper.updateById(notice);
     }
 
+    //删除公告
     public int delNotice(int id) {
-        return dao.delNotice(id);
+        return mapper.deleteById(id);
     }
 
+    //通过title进行模糊获取
     public int getCount(String name) {
         return dao.getCount(name);
     }
 
+    //通过title模糊,分页查询公告
     public List<Notice> findNotice(int page, int limit, String name) {
         return dao.findNotice(page,limit,name);
     }
 
+    //查询status为0的公告
     public Notice getNotice() {
         return dao.getNotice();
     }
