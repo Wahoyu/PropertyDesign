486c11927ef77b481279190132cef7fb8daacfc7
wahoyu <645702818@qq.com>
2023-05-17 13:18:34 +0800

    5.17修改Room结束

--- src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
@@ -2,14 +2,11 @@ package com.example.demo.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.demo.eneity.Building;
 import com.example.demo.eneity.Room;
-import com.example.demo.dao.RoomDao;
 import com.example.demo.eneity.Unit;
 import com.example.demo.mapper.RoomMapper;
 import com.example.demo.mapper.UnitMapper;
 import com.example.demo.service.RoomService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -17,8 +14,6 @@ import java.util.List;
 
 @Service
 public class RoomServiceImpl implements RoomService {
-    @Autowired
-    RoomDao dao;
     @Resource
     UnitMapper unitMapper;
     @Resource
@@ -67,21 +62,49 @@ public class RoomServiceImpl implements RoomService {
 
     //通过名称模糊查询,分页查询房屋
     public List<Room> findRoom(int page, int limit, String name) {
-        return dao.findRoom(page,limit,name);
+        //分页查询
+        Page<Room> p = new Page<>(page, limit);
+
+        //构造wrapper
+        QueryWrapper<Room> wrapper2 = new QueryWrapper<>();
+        wrapper2.like("name", name);
+
+        List<Room> list = mapper.selectPage(p,wrapper2).getRecords();
+        System.out.println(list);
+
+        //如果list不为空,添加user
+        if (list!=null){
+            for (Room room:list){
+                QueryWrapper<Unit> wrapper = new QueryWrapper<>();
+                wrapper.eq("id", room.getUnit_id());
+                Unit unit = unitMapper.selectOne(wrapper);
+                room.setUnit(unit);
+            }
+            return list;
+        }else{
+            return null;
+        }
     }
 
     //通过名字模糊查询,查询总数
     public int getCount(String name) {
-        return dao.getCount(name);
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.like("name", name);
+        return Math.toIntExact(mapper.selectCount(wrapper));
     }
 
     //查询某个单元中的所有的空闲房屋
     public List<Room> getAllFreeRooms(int unit_id) {
-        return dao.getAllFreeRooms(unit_id);
+        QueryWrapper<Room> wrapper = new QueryWrapper();
+        wrapper.eq("unit_id", unit_id);
+        wrapper.eq("status", 0);
+        return mapper.selectList(wrapper);
     }
 
     //查询所有的空闲房屋的数量
     public int getFreeCount() {
-        return dao.getFreeCount();
+        QueryWrapper<Room> wrapper = new QueryWrapper();
+        wrapper.eq("status", 0);
+        return Math.toIntExact(mapper.selectCount(wrapper));
     }
 }
