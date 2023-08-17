f532f4d1195576b1aff81be777e4af90a454b5ea
wahoyu <645702818@qq.com>
2023-08-17 16:16:11 +0800

    8.16完善项目信息,抹除个人信息

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("=======================================启动成功=======================================");
+        System.out.println("访问 http://localhost:8081/ 以打开项目");
     }
 
 }
