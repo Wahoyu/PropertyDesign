b017863a3ca6eee0382328e305e48f4546792e83
wahoyu <645702818@qq.com>
2025-05-23 11:22:27 +0800

    给小左修改前存档

--- src/main/java/com/example/demo/mapper/AdminMapper.java
+++ src/main/java/com/example/demo/mapper/AdminMapper.java
@@ -2,8 +2,17 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.eneity.Admin;
+import com.example.demo.eneity.User;
+import org.apache.ibatis.annotations.Many;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.FetchType;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.util.List;
 
 @Mapper
 public interface AdminMapper extends BaseMapper<Admin> {
+
 }
